871d4c07e9a2b80b9f0d4fc8c237cef9e0af907a
Gökhan Gökoğlan <119362605+ggokoglann@users.noreply.github.com>
2023-07-30 22:07:10 +0300

    Add files via upload

---
+++ Currencies/Currencies_mvc/Models/Currency.swift
@@ -0,0 +1,20 @@
+//
+//  Currency.swift
+//  Currencies_mvc
+//
+//  Created by Gökhan Gökoğlan on 3.07.2023.
+//
+
+import Foundation
+import UIKit
+
+struct Currency {
+        var name = String()
+        var price = Float()
+        var image = UIImage()
+        var change = Float()
+        var nameExt = String()
+}
+
+
+
